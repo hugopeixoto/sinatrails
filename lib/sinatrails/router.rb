e65a4bab88d594edc02d142ea310ef4b549b3153
Hugo Peixoto <hugo.peixoto@gmail.com>
2015-01-18 17:44:38 +0000

    Adds default params to router

--- lib/sinatrails/router.rb
+++ lib/sinatrails/router.rb
@@ -1,12 +1,15 @@
 module Sinatrails
   class Router < Struct.new(:sinatra)
-    def add_route method, path, target
+    def add_route method, path, target, opts = {}
       controller, action = target.split '#'
   
       action = action.to_sym
       controller = (controller.camelize + 'Controller').constantize
+
+      defaults = indifferent_params opts.fetch(:defaults, {})
   
       sinatra.send method, path do
+        params.merge! defaults
         controller.new(params, request, headers).execute action
       end
     end
